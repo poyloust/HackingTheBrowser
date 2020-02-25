9aaeedaf0ff189166b622d41a48f4235182aee75
Xinyue <poyloust@gmail.com>
2020-02-24 20:28:43 -0500

    tab automatically close

---
+++ week4/background.js
@@ -0,0 +1,20 @@
+console.log("bg script");
+chrome.tabs.onCreated.addListener(function(cb){
+    console.log("CREATE TABB");
+    // console.log(cb);
+    chrome.tabs.onUpdated.addListener(function(id,info,tab){
+        // console.log(tab);
+        if(info.status == "complete"){
+            console.log(id+ ",\"" +tab.url+"\","+tab.title);
+             setTimeout(function(){
+                 chrome.tabs.get(id,function(currentTab){
+                     console.log(currentTab.active);
+                     if(currentTab.active == false){
+                        console.log("remove tab\"" +currentTab.url+"\","+currentTab.title);
+                        chrome.tabs.remove(id);
+                     }
+                 });
+            }, ); //settime
+        }
+    });
+});
