925497f95912031627c2932d2f6d563df127b7d4
Xinyue <poyloust@gmail.com>
2020-03-01 17:18:13 -0500

    add folder in bkmk bar

--- week6/background.js
+++ week6/background.js
@@ -1,1 +1,19 @@
 console.log("background js running");
+
+//create bookmark root folder for extension
+chrome.bookmarks.search({
+        'title':'SavedForTabOD'
+    },function(matchingFolder){
+    console.log(matchingFolder);
+    if(matchingFolder.length == 0){
+        chrome.bookmarks.create({
+            'title':'SavedForTabOD'
+        },function(newFolder){ //after created
+            console.log("add " + newFolder.title);
+            console.log("id is " + newFolder.id);
+        });
+    }
+    else{
+        console.log("folder already exists");
+    }
+});
