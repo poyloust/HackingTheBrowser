2df0ad59bc46e9ed2fbf20a2e94b4ec42c4f6914
Xinyue <poyloust@gmail.com>
2020-03-01 18:18:49 -0500

    set timer for tabs

--- week6/background.js
+++ week6/background.js
@@ -1,19 +1,48 @@
 console.log("background js running");
 
+var tabodId;
+
 //create bookmark root folder for extension
 chrome.bookmarks.search({
-        'title':'SavedForTabOD'
+        'title':'Saved for TabOD'
     },function(matchingFolder){
     console.log(matchingFolder);
     if(matchingFolder.length == 0){
         chrome.bookmarks.create({
-            'title':'SavedForTabOD'
+            'title':'Saved for TabOD'
         },function(newFolder){ //after created
+            tabodId = newFolder.id;
             console.log("add " + newFolder.title);
-            console.log("id is " + newFolder.id);
+            console.log("id is " + tabodId);
         });
     }
     else{
-        console.log("folder already exists");
+        tabodId = matchingFolder[0].id;
+        console.log("folder already exists, id is " +tabodId);
     }
 });
+
+//timing the tab opening time
+
+var delayInMinutes = 1;
+
+chrome.tabs.onCreated.addListener(function(){
+    console.log("new tab created");
+    chrome.tabs.onUpdated.addListener(function(id,info,tab){
+        // console.log(tab);
+        if(info.status == "complete"){
+            console.log(id+ ",\"" +tab.url+"\","+tab.title);
+            setTimer(id);
+        }
+    });
+});
+
+function setTimer(_id){
+    chrome.alarms.create(_id.toString(),{
+        delayInMinutes
+    });
+    console.log(_id +" set an alarm for "+delayInMinutes+"mins");
+    chrome.alarms.onAlarm.addListener(function(){
+        console.log("1 min alarm");
+    });
+} 
